a233e80e684f34c8701881b8d8c91e132bbfba02
Hannah <hannah.asis@obf.ateneo.edu>
2020-10-26 16:45:21 +0800

    gui for item registry and customer registry setup but not fully functional yet

--- gui.py
+++ gui.py
@@ -76,6 +76,7 @@ Lamount.grid(column=4,row=0)
 
 def goBack():
     itemFrame.grid_remove()
+    win.geometry(f"{width}x{height}")
     f.grid()
 
 back = tk.Button(itemFrameA, text="Go Back", font="Times 18", bg=bgcolor, fg="black",command=goBack)
@@ -90,23 +91,80 @@ itemFrameB.grid()
 regCustomerFrame = Frame(win)
 regCustomerFrame.configure(bg=bgcolor)
 
+#subframes
 regCustomerFrameA = Frame(regCustomerFrame)
 regCustomerFrameA.configure(bg=bgcolor)
 regCustomerFrameB = Frame(regCustomerFrame)
 regCustomerFrameB.configure(bg=bgcolor)
 
+#for A
+#title
+title = tk.Label(regCustomerFrameA, text="Customer Registry", bg=bgcolor, fg="black", font="Times 32")
+title.grid(column=1,row=0)
+
+#search
+searchLabel = tk.Label(regCustomerFrameA, text="Search item: ", bg=bgcolor, fg="black", font="Times 18", borderwidth=1,relief="solid")
+searchLabel.grid(column=0,row=1)
+
+searchBar = tk.Entry(regCustomerFrameA, width=60, font="Times 18")
+searchBar.grid(column=1,row=1)
+
+def search():        
+    cur.execute(f"SELECT * FROM item WHERE description LIKE '%{searchBar.get()}%'")
+    
+    rows = cur.fetchall()
+    
+    i = 0
+    for r in rows:
+        customerID = tk.Label(regCustomerFrameB, text=r[0], bg="white", fg="black", font="Times 12", borderwidth=1,relief="solid")
+        customerID.grid(column=i,row=i)
+        lastName = tk.Label(regCustomerFrameB, text=r[1], bg="white", fg="black", font="Times 12", borderwidth=1,relief="solid")
+        lastName.grid(column=i+1,row=i)
+        givenName = tk.Label(regCustomerFrameB, text=r[2], bg="white", fg="black", font="Times 12", borderwidth=1,relief="solid")
+        givenName.grid(column=i+2,row=i)
+        middleInitial = tk.Label(regCustomerFrameB, text=r[3], bg="white", fg="black", font="Times 12", borderwidth=1,relief="solid")
+        middleInitial.grid(column=i+3,row=i)
+        address = tk.Label(regCustomerFrameB, text=r[4], bg="white", fg="black", font="Times 12", borderwidth=1,relief="solid")
+        address.grid(column=i+4,row=i)
+        city = tk.Label(regCustomerFrameB, text=r[5], bg="white", fg="black", font="Times 12", borderwidth=1,relief="solid")
+        city.grid(column=i+5,row=i)
+        mobile = tk.Label(regCustomerFrameB, text=r[6], bg="white", fg="black", font="Times 12", borderwidth=1,relief="solid")
+        mobile.grid(column=i+6,row=i)
+        landline = tk.Label(regCustomerFrameB, text=r[7], bg="white", fg="black", font="Times 12", borderwidth=1,relief="solid")
+        landline.grid(column=i+7,row=i)
+        postalCode = tk.Label(regCustomerFrameB, text=r[8], bg="white", fg="black", font="Times 12", borderwidth=1,relief="solid")
+        postalCode.grid(column=i+8,row=i)
+        birthDate = tk.Label(regCustomerFrameB, text=r[9], bg="white", fg="black", font="Times 12", borderwidth=1,relief="solid")
+        birthDate.grid(column=i+9,row=i)
+        age = tk.Label(regCustomerFrameB, text=r[10], bg="white", fg="black", font="Times 12", borderwidth=1,relief="solid")
+        age.grid(column=i+10,row=i)
+        i+=1
+    
+
+searchButton = tk.Button(regCustomerFrameA, text="Search", font="Times 18", bg=bgcolor, fg="black",command=search)
+searchButton.grid(column=2,row=1)
+
 #labels
-LcustomerID = tk.Label(regCustomerFrameB, text="Customer ID", bg="white", fg="black", font="Times 12", borderwidth=1,relief="solid")
-LlastName = tk.Label(regCustomerFrameB, text="Last Name", bg="white", fg="black", font="Times 12", borderwidth=1,relief="solid")
-LgivenName = tk.Label(regCustomerFrameB, text="Given Name", bg="white", fg="black", font="Times 12", borderwidth=1,relief="solid")
-LmiddleInitial = tk.Label(regCustomerFrameB, text="MI", bg="white", fg="black", font="Times 12", borderwidth=1,relief="solid")
-Laddress = tk.Label(regCustomerFrameB, text="Address", bg="white", fg="black", font="Times 12", borderwidth=1,relief="solid")
-Lcity = tk.Label(regCustomerFrameB, text="City", bg="white", fg="black", font="Times 12", borderwidth=1,relief="solid")
-Lmobile = tk.Label(regCustomerFrameB, text="Mobile", bg="white", fg="black", font="Times 12", borderwidth=1,relief="solid")
-Llandline = tk.Label(regCustomerFrameB, text="Landline", bg="white", fg="black", font="Times 12", borderwidth=1,relief="solid")
-LpostalCode = tk.Label(regCustomerFrameB, text="Postal Code", bg="white", fg="black", font="Times 12", borderwidth=1,relief="solid")
-LbirthDate = tk.Label(regCustomerFrameB, text="Birth Date", bg="white", fg="black", font="Times 12", borderwidth=1,relief="solid")
-Lage = tk.Label(regCustomerFrameB, text="Age", bg="white", fg="black", font="Times 12", borderwidth=1,relief="solid")
+customerAttributes = ["Customer ID", "Last Name", "Given Name", "MI", "Address", "City", "Mobile", "Landline", "Postal Code", "Birth Date", "Age"]
+attributeWidths = [10,10,10,5,25,10,10,10,10,10,5]
+
+i = 0
+for a in customerAttributes:
+    label = tk.Label(regCustomerFrameB, text=a, bg="white", fg="black", font="Times 12", borderwidth=1,relief="solid",width=attributeWidths[i])
+    label.grid(column=i,row=0)
+    i+=1
+    
+#back
+def goBack():
+    regCustomerFrame.grid_remove()
+    win.geometry(f"{width}x{height}")
+    f.grid()
+
+back = tk.Button(regCustomerFrameA, text="Go Back", font="Times 18", bg=bgcolor, fg="black",command=goBack)
+back.grid(column=2,row=0)
+
+regCustomerFrameA.grid()
+regCustomerFrameB.grid()
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -119,12 +177,19 @@ newCustomerFrameA.configure(bg=bgcolor)
 #functions
 def item_appraisal():
     f.grid_remove()
+    win.geometry(f"{width}x{height}")
     itemFrame.grid()
+    
+def customer_registry():
+    f.grid_remove()
+    win.geometry("1080x720")
+    regCustomerFrame.grid()    
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 #main frame
 f = Frame(win)
+f.configure(bg=bgcolor)
 
 #title label
 blank = tk.Label(f, text="             ", bg=bgcolor, font="Times 32",width=13)
@@ -137,7 +202,7 @@ itemAppraisalButton = tk.Button(f, text="Find an Item", font="Times 18", bg=bgco
 itemAppraisalButton.grid(column=1,row=1)
 
 #loan computation and releasing
-findCustomer = tk.Button(f, text="Find a Customer", font="Times 18", bg=bgcolor, fg="black",command=item_appraisal)
+findCustomer = tk.Button(f, text="Find a Customer", font="Times 18", bg=bgcolor, fg="black",command=customer_registry)
 findCustomer.grid(column=1,row=2)
 
 newCustomer = tk.Button(f, text="Register a Customer", font="Times 18", bg=bgcolor, fg="black",command=item_appraisal)
